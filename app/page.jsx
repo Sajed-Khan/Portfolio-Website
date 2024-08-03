b75edf9c7465484e1005d478deed8f788d13d8ec
Sajed-Khan <85892336+Sajed-Khan@users.noreply.github.com>
2024-08-03 23:51:20 +0400

    Homepage completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,6 @@
+import Photo from "@/components/Photo";
 import Socials from "@/components/Socials";
+import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import { DownloadIcon } from "lucide-react";
 import Image from "next/image";
@@ -8,7 +10,7 @@ export default function Home() {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br/> <span className="text-accent">Sajed Khan</span>
@@ -24,9 +26,12 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 }
