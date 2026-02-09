77ae794edf3399f40265a5421640741845492bc6
Sajed-Khan <sajed619007@gmail.com>
2026-02-09 12:01:38 +0400

    domain fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const repo = "Portfolio-Website";
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : "",
-  },
 };
 
 export default nextConfig;
